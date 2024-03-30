63856759dafdcb2e955eb0e18d371790794bdc07
jenarkk <158101004+jenarkk@users.noreply.github.com>
2024-03-30 20:40:02 +0000

    Create leapreminder.js

---
+++ features/Dungeons/leapreminder.js
@@ -0,0 +1,16 @@
+import config from "../../config"
+import LocationChecker from "../../utils/locationchecker"
+
+const dungCheck = new LocationChecker("F7", "M7")
+
+register("chat", () => {
+    if(config.leapReminder && dungCheck.check()){
+        ChatLib.say(config.leapReminderText)
+    }
+  }).setCriteria("&r&4[BOSS] Storm&r&c: &r&cI should have known that I stood no chance.&r")
+
+register("chat", () => {
+    if(config.leapReminder && dungCheck.check()){
+        ChatLib.say(config.leapReminderText)
+    }
+  }).setCriteria("&r&4[BOSS] Goldor&r&c: &r&cYOU ARE FACE TO FACE WITH GOLDOR!&r")
