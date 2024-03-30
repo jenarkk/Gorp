bd6fd012e63438ba09a31aedc810037f81733856
jenarkk <158101004+jenarkk@users.noreply.github.com>
2024-03-30 21:30:06 +0000

    Update autokick.js

--- features/General/autokick.js
+++ features/General/autokick.js
@@ -5,3 +5,9 @@ register("chat", () => {
         ChatLib.say("/p kick EZhedeh")
     }
 }).setCriteria("EZhedeh joined the party.").setContains()
+
+register("chat", () => {
+    if(config.autoKick){
+        ChatLib.say("/p kick Redshotz")
+    }
+}).setCriteria("Redshotz joined the party.").setContains()
