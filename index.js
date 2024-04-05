16522a7f948f5251897d64aa40a8d0f2d6f01db0
jenarkk <158101004+jenarkk@users.noreply.github.com>
2024-04-05 04:15:04 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import config from "./config"
 import "./features/Dungeons/leapreminder"
 import "./features/General/autokick"
 import "./features/General/partycommands"
+import "./features/General/gfscommands"
+import "./features/General/autogfs"
 
 register("command", () => {
   config.openGUI()
