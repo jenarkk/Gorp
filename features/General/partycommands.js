951a06f534f50f45ca531da68b398df86705848c
jenarkk <158101004+jenarkk@users.noreply.github.com>
2024-04-05 04:41:31 +0100

    Update partycommands.js

--- features/General/partycommands.js
+++ features/General/partycommands.js
@@ -22,6 +22,12 @@ register("chat", () => {
 
 register("chat", () => {
   if(config.partyCommands){
+    ChatLib.say("https://cdn.discordapp.com/attachments/1017180632859811870/1225466398449467574/IMG_8883.png?ex=66213b85&is=660ec685&hm=ff2020681b1b9e8005e06ef4d68510d6f1542e554d3c0d1a360af823308db68c&")
+  }
+}).setCriteria("-squirrel").setContains()
+
+register("chat", () => {
+  if(config.partyCommands){
     ChatLib.say(`The walrus (Odobenus rosmarus) is a large pinniped marine mammal with discontinuous distribution about the North Pole in the Arctic Ocean and subarctic seas of the Northern Hemisphere.`)
   }
 }).setCriteria("-walrus").setContains()
@@ -34,7 +40,6 @@ register("chat", () => {
 
 register("chat", () => {
   if(config.partyCommands){
-    ChatLib.chat(`${variables.PREFIX}&7Your Ender Pearls have been refilled`)
     ChatLib.say(`My Ender Pearls have been refilled`)
     ChatLib.command("pearlrefill", true)
   }
