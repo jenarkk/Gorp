960d3fa14a1693e50e6f3241494ce570b0ddd9c1
jenarkk <158101004+jenarkk@users.noreply.github.com>
2024-04-05 04:16:35 +0100

    Update leapreminder.js

--- features/Dungeons/leapreminder.js
+++ features/Dungeons/leapreminder.js
@@ -1,10 +1,22 @@
 import config from "../../config"
-import LocationChecker from "../../utils/locationchecker"
+import variables from "../../utils/variables"
+let HealerClass = null
 
-const dungCheck = new LocationChecker("F7", "M7")
+register("chat", (player) => {
+    ChatLib.command(`hset ${player}`, true)
+}).setCriteria("Party Finder > ${player} joined the dungeon group! (Healer").setContains()
 
 register("chat", () => {
-    if(config.leapReminder && dungCheck.check()){
-        ChatLib.say(config.leapReminderText)
+    if(config.leapReminder){
+        ChatLib.say(`Leap to ${HealerClass}`)
     }
 }).setCriteria("&r&4[BOSS] Storm&r&c: &r&cI should have known that I stood no chance.&r")
+
+register("command", () => {
+    ChatLib.chat(`${variables.PREFIX}&7Healer is set to &f${HealerClass}`)
+}).setCommandName("hcheck").setAliases(["healercheck"])
+
+register("command", (arg1) => {
+    HealerClass = arg1
+    ChatLib.chat(`${variables.PREFIX}&7Healer has been set to &f${HealerClass}`)
+}).setCommandName("hset").setAliases(["healerset"])
