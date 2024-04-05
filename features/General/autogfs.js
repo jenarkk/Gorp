77edca91acfe5e27fa86a43b8eec2acc29520f40
jenarkk <158101004+jenarkk@users.noreply.github.com>
2024-04-05 04:19:05 +0100

    Create autogfs.js

---
+++ features/General/autogfs.js
@@ -0,0 +1,23 @@
+import config from "../../config"
+let dead = false
+
+register("chat", () => {
+    dead = true
+}).setCriteria("You can't do this as a Spectator!").setContains()
+
+register("chat", () => {
+    dead = false
+}).setCriteria(`${username} was revived`).setContains()
+
+register("chat", () => {
+    if(config.autoGfs){
+        ChatLib.command("pearlrefill", true)
+    }
+}).setCriteria("&r&aStarting in 1 second.&r").setContains()
+
+register("step", () => {
+    const pearlStack = Player.getInventory().getItems().find(a => a?.getName() == "§fEnder Pearl")
+    if (!pearlStack && config.autoGfsRefill && dead == false) {
+        ChatLib.command(`pearlrefill`, true)
+    }
+}).setDelay("0.5") 
