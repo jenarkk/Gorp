43ab02bf81a87632fe5549f8ee9a0b6321bb95be
jenarkk <158101004+jenarkk@users.noreply.github.com>
2024-04-05 04:16:10 +0100

    Update autokick.js

--- features/General/autokick.js
+++ features/General/autokick.js
@@ -1,13 +1,16 @@
 import config from "../../config"
+import variables from "../../utils/variables"
 
 register("chat", () => {
     if(config.autoKick){
+        ChatLib.chat(`${variables.PREFIX}&7Autokick has kicked EZhedeh`)
         ChatLib.say("/p kick EZhedeh")
     }
 }).setCriteria("EZhedeh joined the party.").setContains()
 
 register("chat", () => {
     if(config.autoKick){
+        ChatLib.chat(`${variables.PREFIX}&7Autokick has kicked Redshotz`)
         ChatLib.say("/p kick Redshotz")
     }
 }).setCriteria("RedShotz joined the party.").setContains()
