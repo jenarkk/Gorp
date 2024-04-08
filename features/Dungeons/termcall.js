75de304121518682ce010c1a4747783f8fbd3084
jenarkk <158101004+jenarkk@users.noreply.github.com>
2024-04-08 22:30:12 +0100

    WHOOPS

--- features/Dungeons/termcall.js
+++ features/Dungeons/termcall.js
@@ -6,9 +6,9 @@ import config from "../../config"
 register("chat", () => {
     if(!config.termCall) return
     if(config,termCall == 0) return
-    if(config,termCall == 1){ChatLib.command("pc Device")}
-    if(config,termCall == 2){ChatLib.command("pc 1")}
-    if(config,termCall == 3){ChatLib.command("pc 2")}
-    if(config,termCall == 4){ChatLib.command("pc 3")}
-    if(config,termCall == 5){ChatLib.command("pc 4")}
+    if(config.termCall == 1){ChatLib.command("pc Device")}
+    if(config.termCall == 2){ChatLib.command("pc 1")}
+    if(config.termCall == 3){ChatLib.command("pc 2")}
+    if(config.termCall == 4){ChatLib.command("pc 3")}
+    if(config.termCall == 5){ChatLib.command("pc 4")}
 }).setCriteria("&r&4[BOSS] Storm&r&c: &r&cI should have known that I stood no chance.&r")
